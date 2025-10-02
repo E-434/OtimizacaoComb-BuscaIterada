1802971c77c8addccfb7e9b4871cf250f4c8bbd0
Emanuel Ruschel Rauter <109447203+E-434@users.noreply.github.com>
2025-10-02 18:35:42 -0300

    Solução inicial
    Programa agora gera uma solução inicial e calcula sua distância - Implementar agora busca local

--- peregrina.jl
+++ peregrina.jl
@@ -45,6 +45,54 @@ function distancia_cartesiana(dest :: Vector{Int}, src :: Vector{Int})
     return floor(Int, dist*100)
 end
 
+#Calcula distância total do caminho (necessário para solução inicial)
+function distancia_total(instancia :: Instancia, caminho :: Vector{Int})
+    #Inicializa distância total
+    dist_total=0
+    #Percorre caminho até penúltimo elemento
+    for i in 1:(instancia.templos-1)
+        #Calcula distância do templo atual para o próximo templo
+        src=caminho[i]
+        dst=caminho[i+1]
+        dist_total+=distancia_cartesiana(instancia.coord[src], instancia.coord[dst])
+    end
+    #Retorna distância total
+    return dist_total
+end
+
+#Gera solução inicial aleatória
+function solucao_inicial(instancia :: Instancia)
+    #Cria vetor com os índices dos templos
+    templos_livres = collect(1:instancia.templos)
+    #Inicializa caminho vazio
+    caminho = Int[]
+    #Enquanto não tiver percorrido todos templos
+    while !isempty(templos_livres)
+        #Cria vetor possiveis que só armazena templos lives cujo todos pré-requisitos já estão no caminho
+        possiveis = [templo for templo in templos_livres if all(pre_req -> pre_req in caminho, instancia.pre_req[templo])]
+        #Escolhe algum templo possível aleatório e adiciona ao caminho
+        escolhido = rand(possiveis)
+        push!(caminho,escolhido)
+        #Remove templo adicionado ao caminho dos templos livres
+        filter!(resto -> resto != escolhido, templos_livres)
+    end
+    return caminho
+end
+
+#Função para busca iterada
+function busca_iterada(instancia :: Instancia, max_iteracoes)
+#Inicialização dos valores
+iteracoes=0
+melhor_caminho=Int[]
+menor_distancia=typemax(Int)
+#Gera caminho inicial
+caminho = solucao_inicial(instancia)
+#Calcula distância inicial
+distancia = distancia_total(instancia,caminho)
+#Imprime solução inicial (teste)
+println("Caminho: ",join(caminho, " "), "\nDistancia: ", distancia)
+end
+
 #Main
 function main()
     #Verifica se usuário inseriu 3 parâmetros obrigatórios
@@ -56,7 +104,12 @@ function main()
     path = ARGS[1]
     max_iteracoes = parse(Int, ARGS[2])
     seed = parse(Int, ARGS[3])
+    #Usa seed para garantir mesma randomização
     Random.seed!(seed)
+    #Lê instância
+    instancia=leitor(path)
+    #Busca iterada(sem tempo)
+    busca_iterada(instancia,100)
 end
 
 main()
